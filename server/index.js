f5f5c1426a74144c8de200a81e1739b96b40b954
Jesus Jaldin <jesusjaldin2002@gmail.com>
2024-10-01 03:26:06 -0400

    add tasks

--- server/index.js
+++ server/index.js
@@ -14,15 +14,18 @@ const io = new Server(server, {
   },
 });
 
+app.use(express.json());
+
+
 io.on("connection", (socket) => {
   console.log("Un cliente se ha conectado");
 
-    // Escuchar cuando un usuario se conecta al tablero
-    socket.on("user-connected", (data) => {
-        console.log(
-          `Usuario conectado a proyecto ${data.projectId}, sprint ${data.sprintId}`
-        );
-      });
+  // Escuchar cuando un usuario se conecta al tablero
+  socket.on("user-connected", (data) => {
+    console.log(
+      `Usuario conectado a proyecto ${data.projectId}, sprint ${data.sprintId}`
+    );
+  });
 
   // Escuchar cuando una tarea se actualiza
   socket.on("task-updated", (data) => {
@@ -31,6 +34,24 @@ io.on("connection", (socket) => {
 
     // Emitir el cambio a todos los demás clientes conectados
     socket.broadcast.emit("task-updated", data);
+  });
+
+  // Escuchar cuando una tarea es añadida
+  socket.on("task-added", (data) => {
+    const { taskId, name, status, description, projectId, sprintId } = data;
+    console.log(
+      `Nueva tarea añadida a proyecto ${projectId}, sprint ${sprintId}`
+    );
+
+    // Emitir el evento solo a los demás clientes conectados
+    socket.broadcast.emit("task-added", {
+      taskId,
+      name,
+      status,
+      description,
+      projectId,
+      sprintId,
+    });
   });
 
   // Escuchar cuando una tarea se elimina
@@ -52,6 +73,30 @@ io.on("connection", (socket) => {
 });
 
 app.use(logger("dev"));
+
+app.post("/task-added", (req, res) => {
+    const { taskId, name, status, description, projectId, sprintId } = req.body;
+  
+    // Validar si todos los datos están presentes
+    if (!taskId || !name || !status || !projectId || !sprintId) {
+      return res.status(400).send({ success: false, message: "Faltan datos" });
+    }
+  
+    console.log(`Nueva tarea añadida: ${name}, proyecto ${projectId}, sprint ${sprintId}`);
+  
+    // Emitir el evento 'task-added' a todos los clientes conectados
+    io.emit("task-added", {
+      taskId,
+      name,
+      status,
+      description,
+      projectId,
+      sprintId,
+    });
+  
+    // Enviar una respuesta de éxito
+    res.status(200).send({ success: true, message: "Tarea añadida y emitida correctamente" });
+  });
 
 // Habilitar CORS para todas las solicitudes
 app.use(
